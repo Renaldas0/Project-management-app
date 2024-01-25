317b481815eeb1c2843bd7a89bca0baf38ef9cd3
Renaldas Bendikas <97538312+Renaldas0@users.noreply.github.com>
2024-01-25 19:27:09 +0000

    add no project selected component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Sidebar from './components/Sidebar';
 import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +9,7 @@ function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   )
 }
