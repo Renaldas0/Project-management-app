7a348fbf97b3225555138bac88d5194c7a3c4c41
Renaldas Bendikas <renaldasb1999@gmail.com>
2024-01-25 17:11:25 +0000

    add input and newproject components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Sidebar from './components/Sidebar';
+import NewProject from './components/NewProject';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
+      <NewProject />
     </main>
   )
 }
