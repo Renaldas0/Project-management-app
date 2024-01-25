78793b562d6d91e9e26b4d4b6bc9cf46934c4d3a
Renaldas Bendikas <97538312+Renaldas0@users.noreply.github.com>
2024-01-25 20:56:13 +0000

    add new project component update refs and link up save btn

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,47 @@ import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  function handleSaveProject(projectData) {
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  console.log(projectsState);
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onSave={handleSaveProject} />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onAddProject={handleAddProject} />
+  };
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NoProjectSelected />
+      <Sidebar onAddProject={handleAddProject} />
+      {content}
     </main>
   )
 }
