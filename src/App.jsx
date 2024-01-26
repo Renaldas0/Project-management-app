ca715708ba089cc301dba13b1563bb31f6d6a63e
Renaldas Bendikas <renaldasb1999@gmail.com>
2024-01-26 12:02:05 +0000

    add selectedProject comp

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react';
 import Sidebar from './components/Sidebar';
 import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
+import SelectedProject from './components/SelectedProject';
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: []
   });
+
+  function handleSelectProject(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
 
   function handleAddProject() {
     setProjectsState(prevState => {
@@ -41,20 +51,27 @@ function App() {
     });
   }
 
-  let content;
+  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
+
+  let content = <SelectedProject project={selectedProject} />;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onSave={handleSaveProject} onCancel={handleCancelProject} />
+    content = (
+      <NewProject onSave={handleSaveProject} onCancel={handleCancelProject} />
+    )
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onAddProject={handleAddProject} />
   };
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar onAddProject={handleAddProject} projects={projectsState.projects} />
+      <Sidebar
+        onAddProject={handleAddProject}
+        projects={projectsState.projects}
+        onSelectProject={handleSelectProject} />
       {content}
     </main>
   )
 }
 
-export default App
+export default App;
